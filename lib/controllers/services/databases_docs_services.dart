2e4f76a15c84e05943cfff77412f55bf85ba8d7b
mourichonT <47600724+mourichonT@users.noreply.github.com>
2024-07-01 00:15:26 +0200

    finalisation pages et onglet

--- lib/controllers/services/databases_docs_services.dart
+++ lib/controllers/services/databases_docs_services.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:connect_kasa/models/pages_models/document_model.dart';
+import 'package:connect_kasa/models/pages_models/lot.dart';
 
 class DataBasesDocsServices {
   final FirebaseFirestore db = FirebaseFirestore.instance;
@@ -28,5 +29,67 @@ class DataBasesDocsServices {
 
     // Retourner le nouveau document, même s'il n'a pas été ajouté à la base de données
     return newDoc;
+  }
+
+  Future<List<DocumentModel>> getAllDocs(String residenceId) async {
+    List<DocumentModel> docs = [];
+
+    try {
+      QuerySnapshot<Map<String, dynamic>> querySnapshot = await db
+          .collection("Residence")
+          .doc(residenceId)
+          .collection("documents_copro")
+          .get();
+      for (var docSnapshot in querySnapshot.docs) {
+        // Convertir chaque document en objet Post
+        docs.add(DocumentModel.fromJson(docSnapshot.data()));
+      }
+    } catch (e) {
+      print("Error completing in getAllDocs: $e");
+    }
+
+    return docs;
+  }
+
+  Future<List<DocumentModel>> getDocByUser(
+      String residenceId, String lotId, List<String> numUser) async {
+    List<DocumentModel> docs = [];
+
+    try {
+      print("Début de la fonction getDocByUser");
+
+      // Récupérer la référence de la collection "Residence"
+      CollectionReference residenceRef =
+          FirebaseFirestore.instance.collection("Residence");
+
+      // Récupérer le document de la résidence spécifique
+      DocumentReference residenceDocRef = residenceRef.doc(residenceId);
+
+      // Récupérer la collection "lot" pour la résidence spécifique
+      QuerySnapshot lotQuerySnapshot = await residenceDocRef
+          .collection("lot")
+          .where("refLot", isEqualTo: lotId)
+          .get();
+
+      // Parcourir chaque document de la collection "lot"
+      for (QueryDocumentSnapshot lotDoc in lotQuerySnapshot.docs) {
+        // Récupérer les documents de chaque lot et filtrer par "numUser"
+        QuerySnapshot docQuerySnapshot = await lotDoc.reference
+            .collection("documents")
+            .where("destinataire", arrayContainsAny: numUser)
+            .get();
+
+        for (QueryDocumentSnapshot doc in docQuerySnapshot.docs) {
+          Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+
+          DocumentModel document = DocumentModel.fromJson(data);
+          docs.add(document);
+        }
+      }
+    } catch (e) {
+      print("Erreur dans getDocByUser : $e");
+    }
+
+    return docs;
   }
 }
